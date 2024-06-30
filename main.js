b3388383e097b9a271bbca0879dd34ed94b1b159
Jeongsunga <jsa3341@gmail.com>
2024-06-30 23:16:53 +0900

    finish function

--- main.js
+++ main.js
@@ -9,26 +9,63 @@
 // 유저가 이미 입력한 숫자를 입력하면 알려준다(기회를 깎지 않는다)
 
 let computerNum = 0
+let chances = 5 // 1) 10으로 변경
+let gameOver = false
 let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
+let resetButton = document.getElementById("reset-button")
+let chanceArea = document.getElementById("chance-area")
+let history = []
 
 playButton.addEventListener("click", play)  // 함수를 매개변수로 사용
+resetButton.addEventListener("click", reset)
+userInput.addEventListener("focus", function(){userInput.value = ""})
 
 function pickRandomNum(){
-    computerNum = Math.floor(Math.random() * 100) + 1 // 0 ~ 1까지 반환
+    computerNum = Math.floor(Math.random() * 100) + 1 // 4) 곱하는 숫자를 10으로 변경
     console.log(computerNum)
 }
 
 function play(){
     let userValue = userInput.value
+    if(userValue < 1 || userValue > 100){
+        resultArea.textContent = "1과 100 사이 숫자를 입력하시오."
+        return;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력하시오."
+        return;
+    }
+    chances --
+    console.log("chance", chances)
+    chanceArea.textContent = `남은 기회 : ${chances} 번`
     if(userValue < computerNum){
         resultArea.textContent = "Up!!"
     } else if (userValue > computerNum){
         resultArea.textContent = "Down!!"
     } else{
         resultArea.textContent = "Correct!!!"
+        gameOver = true  // 2) 숫자 맞춘 후 버튼 disable
     }
+
+    history.push(userValue)
+
+    if(chances < 1){
+        gameOver = true
+    }
+    if(gameOver){   // 3) gameOver == true과 동일 -> if 안의 조건문이 true라면 실행되므로
+        playButton.disabled = true
+    }
+}
+
+function reset(){
+    // user input 창이 깨끗하게 정리
+    userInput.value = ""
+    // 새로운 번호로 생성
+    pickRandomNum()
+    resultArea.textContent = "결과 값 확인하기"
 }
 
 pickRandomNum()
