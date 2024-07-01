311af5c269f0456ce1c3bd3d3f9b00c3baa209e4
Jeongsunga <jsa3341@gmail.com>
2024-07-01 18:28:16 +0900

    make UI

--- main.js
+++ main.js
@@ -9,33 +9,45 @@
 // 유저가 이미 입력한 숫자를 입력하면 알려준다(기회를 깎지 않는다)
 
 let computerNum = 0
-let chances = 5 // 1) 10으로 변경
+let chances = 3 // 1) 10으로 변경
 let gameOver = false
 let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
 let resetButton = document.getElementById("reset-button")
+let hintButton = document.getElementById("hint-button")
 let chanceArea = document.getElementById("chance-area")
 let history = []
 
+const startImg = "./image/start.jpg"
+const correctImg = "./image/correct.gif"
+const failImg = "./image/fail.gif"
+const upImg = "./image/up.gif"
+const downImg = "./image/down.gif"
+let image = document.querySelector('.gif-area')
+image.src = startImg;
+
 playButton.addEventListener("click", play)  // 함수를 매개변수로 사용
 resetButton.addEventListener("click", reset)
+hintButton.addEventListener("focus", hint)
 userInput.addEventListener("focus", function(){userInput.value = ""})
 
 function pickRandomNum(){
     computerNum = Math.floor(Math.random() * 100) + 1 // 4) 곱하는 숫자를 10으로 변경
-    console.log(computerNum)
+    return computerNum
 }
 
 function play(){
     let userValue = userInput.value
     if(userValue < 1 || userValue > 100){
         resultArea.textContent = "1과 100 사이 숫자를 입력하시오."
+        image.src = failImg;
         return;
     }
 
     if(history.includes(userValue)){
         resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력하시오."
+        image.src = failImg;
         return;
     }
     chances --
@@ -43,10 +55,13 @@ function play(){
     chanceArea.textContent = `남은 기회 : ${chances} 번`
     if(userValue < computerNum){
         resultArea.textContent = "Up!!"
+        image.src = upImg;
     } else if (userValue > computerNum){
         resultArea.textContent = "Down!!"
+        image.src = downImg;
     } else{
         resultArea.textContent = "Correct!!!"
+        image.src = correctImg;
         gameOver = true  // 2) 숫자 맞춘 후 버튼 disable
     }
 
@@ -54,6 +69,7 @@ function play(){
 
     if(chances < 1){
         gameOver = true
+        image.src = failImg;
     }
     if(gameOver){   // 3) gameOver == true과 동일 -> if 안의 조건문이 true라면 실행되므로
         playButton.disabled = true
@@ -66,6 +82,17 @@ function reset(){
     // 새로운 번호로 생성
     pickRandomNum()
     resultArea.textContent = "결과 값 확인하기"
+    chances = 3
+    chanceArea.textContent = `남은 기회 : ${chances} 번`
+    history = []
+    gameOver = false
+    playButton.disabled = false
+    image.src = startImg;
+}
+
+function hint(){
+    let hintNum = pickRandomNum()
+    resultArea.textContent = hintNum
 }
 
 pickRandomNum()
